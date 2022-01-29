67bc6f076bdcd6b1cd5f73ced3a7dc1a9472d630
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-29 00:29:25 -1000

    will continue to work on js table/nodes

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+
+INSERT INTO employee (id,first_name, last_name, role_id, manager_id)
+VALUES 
+('101','Cassie', 'Cooper', '100', '100'),
+('101','Paxton', 'Lee', '103', '100'),
+('202','Stephen', 'Roe', '204', '200'),
+('202','Daniel', 'Fargus', '200', '200'),
+('303','Cherry', 'von Howztinski', '300', '300'),
+('404','Jackie', 'Daytona', '400', '400'),
+('404','Nandilia', 'Billings', '402', '400'),
+('505','Jenny', 'Aruokay', '500', '500');
+
+
+INSERT INTO department(id, name)
+VALUES 
+('101','Engineering'),
+('202','Administrations'),
+('303','Human Resources'),
+('404','Social Media Team'),
+('505','Sales');
+
+INSERT INTO role(id,title,salary,department_id)
+VALUES
+('100','Lead Engineer','160000','101'),
+('103','Software Engineer','140000','101'),
+('204','Front Desk Greeter','100000','202'),
+('200','Office Manager','160000','202'),
+('300','HR Manager','160000','303'),
+('400','Social Media Coordinator','170000','404'),
+('402','Social Media Host','120000','404'),
+('500','Sales Manager','170000','505');
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,23 @@
-USE company;
+CREATE DATABASE blackcat_db;
+USE blackcat_db;
 
-CREATE TABLE directory (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    department VARCHAR(30) NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    manager_id VARCHAR(30) NOT NULL
-    
+CREATE TABLE department (
+    id INIT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
-INSERT INTO directory (first_name, last_name, department, title, salary, manager_id)
-VALUES
-('Jane', 'Doe', 'Administrations', 'Front Desk', '80.000', 'Becky Apples'),
-('John','Doe','Sales','Salesperson','80.000','Becky Apples'),
-('Jackie','Daytona','Legal','Lawyer','200.000','Self'),
-('Jennifer','Peters','Engineering','Lead Engineer','160.000','Self'),
-('Peter','Parker','Engineering','Engineer','120.000','Jennifer Peters');
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
 
-SELECT * From directory;
+CREATE TABLE employee(
+	id INT,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	role_id INT,
+	manager_id INT NULL
+);
 
-DELETE FROM directory
-WHERE ID=10;
