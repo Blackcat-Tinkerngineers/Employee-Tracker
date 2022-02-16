407a842a5a26dee6fc268c5e29602e4fde1941a3
Chelsie Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-02-15 22:54:42 -1000

    db update

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,26 @@
+CREATE DATABASE blackcat_db;
+USE blackcat_db;
+
+CREATE TABLE department (
+    id INT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT
+);
+
+CREATE TABLE employee(
+    id INT,
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+    employee_id INT,
+	manager_id INT
+);
+
 INSERT INTO employee (id, first_name, last_name, employee_id, manager_id)
 VALUES 
 ('101','Cassie', 'Cooper', '100', '100'),
